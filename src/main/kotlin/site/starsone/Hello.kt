a7542e7f0e0ba3dde6788ab6846d2b0499687a34
starone <starsone@Foxmail.com>
2022-07-16 21:52:18 +0800

    :tada: 初始化项目

---
+++ src/main/kotlin/site/starsone/Hello.kt
@@ -0,0 +1,6 @@
+package site.starsone
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/site/starsone/HelloTest.kt
@@ -0,0 +1,8 @@
+package site.starsone
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
